55b8faa6fd5b48cd3d5a3cfad5455d14fd21c82f
agusmakmun <summon.agus@gmail.com>
2018-08-30 21:25:34 +0700

    typo

--- ebayscraper/wsgi.py
+++ ebayscraper/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ebayscrapper project.
+WSGI config for ebayscraper project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebayscrapper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebayscraper.settings')
 
 application = get_wsgi_application()
